8b07c77008e7d0c662bea763b07fa78023276706
Elmer Regalado <elma_regaladoc@unicah.edu>
2023-11-28 16:14:35 -0600

    Listo

--- Sistema_Inventario/Formularios/FrmCliente.Designer.cs
+++ Sistema_Inventario/Formularios/FrmCliente.Designer.cs
@@ -28,31 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            BtnNuevo = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCliente));
+            dataGridView1 = new DataGridView();
+            btnagregar = new Button();
+            btnlimpiar = new Button();
+            btnuevo = new Button();
+            btnsalir = new Button();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            lblcodigo = new Label();
+            lblnombre = new Label();
+            lblapellido = new Label();
+            lbltelefono = new Label();
+            lblemail = new Label();
+            lblsalario = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // BtnNuevo
+            // dataGridView1
             // 
-            BtnNuevo.Location = new Point(343, 205);
-            BtnNuevo.Name = "BtnNuevo";
-            BtnNuevo.Size = new Size(114, 40);
-            BtnNuevo.TabIndex = 25;
-            BtnNuevo.Text = "Nuevo";
-            BtnNuevo.UseVisualStyleBackColor = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(206, 124);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(527, 268);
+            dataGridView1.TabIndex = 14;
+            // 
+            // btnagregar
+            // 
+            btnagregar.Location = new Point(231, 416);
+            btnagregar.Name = "btnagregar";
+            btnagregar.Size = new Size(75, 23);
+            btnagregar.TabIndex = 15;
+            btnagregar.Text = "Agregar";
+            btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // btnlimpiar
+            // 
+            btnlimpiar.Location = new Point(355, 412);
+            btnlimpiar.Name = "btnlimpiar";
+            btnlimpiar.Size = new Size(75, 23);
+            btnlimpiar.TabIndex = 16;
+            btnlimpiar.Text = "Limpiar";
+            btnlimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnuevo
+            // 
+            btnuevo.Location = new Point(492, 412);
+            btnuevo.Name = "btnuevo";
+            btnuevo.Size = new Size(75, 23);
+            btnuevo.TabIndex = 17;
+            btnuevo.Text = "Nuevo";
+            btnuevo.UseVisualStyleBackColor = true;
+            // 
+            // btnsalir
+            // 
+            btnsalir.Location = new Point(658, 412);
+            btnsalir.Name = "btnsalir";
+            btnsalir.Size = new Size(75, 23);
+            btnsalir.TabIndex = 18;
+            btnsalir.Text = "Salir";
+            btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(164, 133);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 19;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(76, 174);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(76, 226);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(76, 273);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(76, 329);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 23;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(76, 369);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 24;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(76, 420);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 25;
+            // 
+            // lblcodigo
+            // 
+            lblcodigo.AutoSize = true;
+            lblcodigo.Location = new Point(2, 174);
+            lblcodigo.Name = "lblcodigo";
+            lblcodigo.Size = new Size(52, 15);
+            lblcodigo.TabIndex = 26;
+            lblcodigo.Text = " Codigo:";
+            // 
+            // lblnombre
+            // 
+            lblnombre.AutoSize = true;
+            lblnombre.Location = new Point(2, 226);
+            lblnombre.Name = "lblnombre";
+            lblnombre.Size = new Size(54, 15);
+            lblnombre.TabIndex = 27;
+            lblnombre.Text = "Nombre:";
+            // 
+            // lblapellido
+            // 
+            lblapellido.AutoSize = true;
+            lblapellido.Location = new Point(2, 273);
+            lblapellido.Name = "lblapellido";
+            lblapellido.Size = new Size(54, 15);
+            lblapellido.TabIndex = 28;
+            lblapellido.Text = "Apellido:";
+            // 
+            // lbltelefono
+            // 
+            lbltelefono.AutoSize = true;
+            lbltelefono.Location = new Point(-1, 329);
+            lbltelefono.Name = "lbltelefono";
+            lbltelefono.Size = new Size(55, 15);
+            lbltelefono.TabIndex = 29;
+            lbltelefono.Text = "Telefono:";
+            // 
+            // lblemail
+            // 
+            lblemail.AutoSize = true;
+            lblemail.Location = new Point(12, 369);
+            lblemail.Name = "lblemail";
+            lblemail.Size = new Size(39, 15);
+            lblemail.TabIndex = 30;
+            lblemail.Text = "Email:";
+            // 
+            // lblsalario
+            // 
+            lblsalario.AutoSize = true;
+            lblsalario.Location = new Point(6, 420);
+            lblsalario.Name = "lblsalario";
+            lblsalario.Size = new Size(45, 15);
+            lblsalario.TabIndex = 31;
+            lblsalario.Text = "Salario:";
             // 
             // FrmCliente
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(BtnNuevo);
+            ClientSize = new Size(806, 491);
+            Controls.Add(lblsalario);
+            Controls.Add(lblemail);
+            Controls.Add(lbltelefono);
+            Controls.Add(lblapellido);
+            Controls.Add(lblnombre);
+            Controls.Add(lblcodigo);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnsalir);
+            Controls.Add(btnuevo);
+            Controls.Add(btnlimpiar);
+            Controls.Add(btnagregar);
+            Controls.Add(dataGridView1);
             Name = "FrmCliente";
             Text = "FrmCliente";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button BtnNuevo;
+        private DataGridView dataGridView1;
+        private Button btnagregar;
+        private Button btnlimpiar;
+        private Button btnuevo;
+        private Button btnsalir;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label lblcodigo;
+        private Label lblnombre;
+        private Label lblapellido;
+        private Label lbltelefono;
+        private Label lblemail;
+        private Label lblsalario;
     }
 }
--- Sistema_Inventario/Formularios/FrmCompras.Designer.cs
+++ Sistema_Inventario/Formularios/FrmCompras.Designer.cs
@@ -28,12 +28,239 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmCompras";
+            lblnombreproducto = new Label();
+            lblprecio = new Label();
+            lblcantidad = new Label();
+            lbltotalapagar = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            btnagregar = new Button();
+            btnlimpiar = new Button();
+            btnuevo = new Button();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            textBox7 = new TextBox();
+            lblproductos = new Label();
+            textBox8 = new TextBox();
+            lBLIGV = new Label();
+            textBox9 = new TextBox();
+            lblimporte = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblnombreproducto
+            // 
+            lblnombreproducto.AutoSize = true;
+            lblnombreproducto.Location = new Point(102, 53);
+            lblnombreproducto.Name = "lblnombreproducto";
+            lblnombreproducto.Size = new Size(109, 15);
+            lblnombreproducto.TabIndex = 0;
+            lblnombreproducto.Text = " Nombre Producto:";
+            // 
+            // lblprecio
+            // 
+            lblprecio.AutoSize = true;
+            lblprecio.Location = new Point(100, 106);
+            lblprecio.Name = "lblprecio";
+            lblprecio.Size = new Size(43, 15);
+            lblprecio.TabIndex = 1;
+            lblprecio.Text = "Precio:";
+            // 
+            // lblcantidad
+            // 
+            lblcantidad.AutoSize = true;
+            lblcantidad.Location = new Point(100, 167);
+            lblcantidad.Name = "lblcantidad";
+            lblcantidad.Size = new Size(58, 15);
+            lblcantidad.TabIndex = 2;
+            lblcantidad.Text = "Cantidad:";
+            // 
+            // lbltotalapagar
+            // 
+            lbltotalapagar.AutoSize = true;
+            lbltotalapagar.Location = new Point(102, 124);
+            lbltotalapagar.Name = "lbltotalapagar";
+            lbltotalapagar.Size = new Size(77, 15);
+            lbltotalapagar.TabIndex = 3;
+            lbltotalapagar.Text = "Total a Pagar:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(289, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(244, 23);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(289, 103);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(289, 159);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(289, 124);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // btnagregar
+            // 
+            btnagregar.Location = new Point(114, 236);
+            btnagregar.Name = "btnagregar";
+            btnagregar.Size = new Size(75, 23);
+            btnagregar.TabIndex = 14;
+            btnagregar.Text = "Agregar";
+            btnagregar.UseVisualStyleBackColor = true;
+            // 
+            // btnlimpiar
+            // 
+            btnlimpiar.Location = new Point(301, 227);
+            btnlimpiar.Name = "btnlimpiar";
+            btnlimpiar.Size = new Size(75, 23);
+            btnlimpiar.TabIndex = 15;
+            btnlimpiar.Text = "Limpiar";
+            btnlimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnuevo
+            // 
+            btnuevo.Location = new Point(494, 227);
+            btnuevo.Name = "btnuevo";
+            btnuevo.Size = new Size(75, 23);
+            btnuevo.TabIndex = 16;
+            btnuevo.Text = "Nuevo";
+            btnuevo.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(lblnombreproducto);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(lblprecio);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(lblcantidad);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(764, 209);
+            groupBox1.TabIndex = 18;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ingresar Datos";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(lblproductos);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Controls.Add(lBLIGV);
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(lblimporte);
+            groupBox2.Controls.Add(lbltotalapagar);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Location = new Point(12, 265);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(764, 173);
+            groupBox2.TabIndex = 19;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Resultados";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(289, 16);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(244, 23);
+            textBox7.TabIndex = 6;
+            // 
+            // lblproductos
+            // 
+            lblproductos.AutoSize = true;
+            lblproductos.Location = new Point(102, 19);
+            lblproductos.Name = "lblproductos";
+            lblproductos.Size = new Size(67, 15);
+            lblproductos.TabIndex = 0;
+            lblproductos.Text = " Productos:";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(289, 77);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 23);
+            textBox8.TabIndex = 7;
+            // 
+            // lBLIGV
+            // 
+            lBLIGV.AutoSize = true;
+            lBLIGV.Location = new Point(102, 80);
+            lBLIGV.Name = "lBLIGV";
+            lBLIGV.Size = new Size(53, 15);
+            lBLIGV.TabIndex = 1;
+            lBLIGV.Text = "IGV 18%:";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(289, 45);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 23);
+            textBox9.TabIndex = 8;
+            // 
+            // lblimporte
+            // 
+            lblimporte.AutoSize = true;
+            lblimporte.Location = new Point(102, 48);
+            lblimporte.Name = "lblimporte";
+            lblimporte.Size = new Size(119, 15);
+            lblimporte.TabIndex = 2;
+            lblimporte.Text = "Importe de Compras:";
+            // 
+            // FrmCompras
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(btnuevo);
+            Controls.Add(btnlimpiar);
+            Controls.Add(btnagregar);
+            Name = "FrmCompras";
+            Text = "FrmCliente";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label lblnombreproducto;
+        private Label lblprecio;
+        private Label lblcantidad;
+        private Label lbltotalapagar;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button btnagregar;
+        private Button btnlimpiar;
+        private Button btnuevo;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private TextBox textBox7;
+        private Label lblproductos;
+        private TextBox textBox8;
+        private Label lBLIGV;
+        private TextBox textBox9;
+        private Label lblimporte;
     }
 }
