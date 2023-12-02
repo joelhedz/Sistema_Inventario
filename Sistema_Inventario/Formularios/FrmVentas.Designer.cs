7be3555875bf806165a23cac6a24f65d7fbf8396
CristhopherNu <cristhophernunez3@gmail.com>
2023-12-02 00:52:48 -0600

    estan el boton buscar de ventas  y anular venta

--- Sistema_Inventario/Formularios/FrmVentas.Designer.cs
+++ Sistema_Inventario/Formularios/FrmVentas.Designer.cs
@@ -22,11 +22,11 @@
         private Label label4;
         private Button btnbusacr;
         private Button bntmenu;
-        private DateTimePicker dateTimePicker;
+        private DateTimePicker dateTimePickerinicio;
         private Button button2;
         private GroupBox groupBox1;
         private Label label1;
-        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePickerfinal;
         private Button BtnBuscar;
         private Button BtnAnular;
         private DataGridView dataGridView2;
---
+++ Sistema_Inventario/Formularios/Sistema_Inventario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Inventario.Formularios
+{
+    public partial class Sistema_Inventario : Form
+    {
+        public Sistema_Inventario()
+        {
+            InitializeComponent();
+        }
+
+        private void Sistema_Inventario_Load(object sender, EventArgs e)
+        {
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("DataSetVentas", datos));
+            reportViewer1.LocalReport.ReportEmbeddedResource = "Sistema_Inventario.ReporteVentas.rdlc";
+            reportViewer1.RefreshReport();
+        }
+    }
+}
